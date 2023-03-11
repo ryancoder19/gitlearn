34df5d01a035e9d144d38b9eb5da3fbf8e47941a
Aryan Kumar <aryan.ar201881@gmail.com>
2023-03-11 21:33:43 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+  int first_number, second_number, mul;
+    
+  cout << "Enter two integers: ";
+  cin >> first_number >> second_number;
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  mul = first_number * second_number;
+
+  // prints sum 
+  cout << first_number << " * " <<  second_number << " = " << mul<< endl;   
+  cout<<"This is ans";  
+  return 0;
+}
